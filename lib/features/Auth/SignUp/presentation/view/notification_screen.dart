02ec8cb1a9db38fe7beeaeca3cba258e967f6a20
omar salah <96392338+Omar-Salah17@users.noreply.github.com>
2024-08-25 14:12:13 +0300

    notification screen

---
+++ lib/features/Auth/SignUp/presentation/view/notification_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:negmt_heliopolis/core/utlis/theming/styles.dart';
+import 'package:negmt_heliopolis/features/Auth/SignUp/presentation/view/widgets/sign_up_custom_button.dart';
+
+class NotificationScreen extends StatelessWidget {
+  const NotificationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 60.h),
+          child: Column(
+            children: [
+              AppBar(
+                elevation: 0,
+                backgroundColor: Colors.transparent,
+                leading: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: Icon(Icons.arrow_back, size: 33.sp)),
+              ),
+              SizedBox(
+                height: 50.h,
+              ),
+              Image.asset("assets/Icons_logos/notification_logo.png",),
+
+              SizedBox(
+                height: 20.h,
+              ),
+              Text(
+                "Allow Us To Send You ",
+                style: Styles.styles25w600black,
+              ),
+              SizedBox(
+                height: 5.h,
+              ),
+              Text(
+                "notifications?",
+                style: Styles.styles25w600Black,
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              SignUpCustomButton(
+                  buttonText: "Allow Notification", onPressed: () {}),
+              SizedBox(
+                height: 15.h,
+              ),
+                 SignUpCustomButton(
+                  buttonText: "Maybe Later",
+                  onPressed: () {
+                  
+                  },
+                  buttonColor: const Color.fromRGBO(0, 126, 143, 0.1),
+                  textColor: const Color.fromRGBO(0, 126, 143, 1),
+                )
+      
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
