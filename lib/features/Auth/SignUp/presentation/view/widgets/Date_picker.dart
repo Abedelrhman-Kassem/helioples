da71d6a25161b36b761ba36bba76ee91c7e48eb2
omar salah <96392338+Omar-Salah17@users.noreply.github.com>
2024-08-25 14:07:52 +0300

    reusable widgets for sign up flow

---
+++ lib/features/Auth/SignUp/presentation/view/widgets/Date_picker.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:negmt_heliopolis/core/utlis/theming/colors.dart';
+import 'package:negmt_heliopolis/core/utlis/theming/styles.dart';
+
+
+class DatePicker extends StatefulWidget {
+  final String labelText;
+  //final void Function(DateTime) onDateSelected;
+  final String? Function(String?)? validator;
+
+  const DatePicker({
+    super.key,
+    required this.labelText,
+    //required this.onDateSelected,
+    this.validator,
+  });
+
+  @override
+  State<DatePicker> createState() => _DatePickerState();
+}
+
+class _DatePickerState extends State<DatePicker> {
+  final TextEditingController _dateController = TextEditingController();
+  late String selectedDate;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedDate = '';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _selectDate();
+      },
+      child: AbsorbPointer(
+        absorbing: true,
+        child: SizedBox(
+          child: TextFormField(
+            
+            controller: _dateController,
+            validator: widget.validator,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: const Color.fromRGBO(246, 246, 246, 1), 
+              iconColor: MyColors.mainColor,
+              focusColor: MyColors.mainColor,
+              hintText: widget.labelText,
+              suffixIcon: Padding(
+                padding: const EdgeInsets.all(12.0), 
+                child: SvgPicture.asset(
+                  'assets/svg_icons/calendar.svg', 
+
+                ),
+              ),
+                enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25.r),
+              borderSide: const BorderSide(
+                color: Color.fromRGBO(210, 210, 210, 1),
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30.r),
+              borderSide: const BorderSide(
+                color: Color.fromRGBO(210, 210, 210, 1),
+                width: 1.5,
+              ),
+            ),
+             contentPadding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 14.h),
+
+            ),
+               style: Styles.styles17w500NormalBlack,
+            readOnly: true,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _selectDate() async {
+    DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime.now(),
+      builder: (context, child) => Theme(
+        data: ThemeData().copyWith(
+          colorScheme: const ColorScheme.light(
+            primary: Color.fromRGBO(0, 126, 143, 1),
+            onPrimary: Colors.white,
+            surface: Colors.white,
+          ),
+        ),
+        child: child ?? Container(),
+      ),
+    );
+    if (picked != null) {
+      setState(() {
+        _dateController.text = DateFormat('M/d/yyyy').format(picked);
+        //widget.onDateSelected(picked);
+      });
+    }
+  }
+}
---
+++ lib/features/Auth/SignUp/presentation/view/widgets/nh_logo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LogoWidget extends StatelessWidget {
+  const LogoWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      "assets/Icons_logos/Helioplis_logo.png",
+      width: 95.w,
+      height: 95.h,
+    );
+  }
+}
