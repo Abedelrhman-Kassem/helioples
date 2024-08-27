3f7a05db880166edf49df434bffa15c4b224dfb9
Abedelrhman-Kassem <Abedelrhman-Kassem>
2024-08-27 13:12:34 +0300

    solve conflicts

--- lib/features/Explore/presentation/view/explore_screen.dart
+++ lib/features/Explore/presentation/view/explore_screen.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:negmt_heliopolis/core/utlis/theming/colors.dart';
+import 'package:negmt_heliopolis/core/utlis/theming/styles.dart';
+import 'package:buttons_tabbar/buttons_tabbar.dart';
+import 'package:negmt_heliopolis/core/widgets/cart_container.dart';
+import 'package:negmt_heliopolis/core/widgets/item_widget_grid.dart';
 
-class ExploreScreen extends StatelessWidget {
+class ExploreScreen extends StatefulWidget {
   const ExploreScreen({super.key});
 
   @override
@@ -85,9 +92,152 @@ class _ExploreScreenState extends State<ExploreScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('hello from explore screen'),
+    return DefaultTabController(
+      length: categories.length,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(180.h),
+            child: Builder(
+              builder: (context) {
+                tabContext = context;
+                return SizedBox(
+                  height: 150.h,
+                  width: double.infinity,
+                  child: AppBar(
+                    toolbarHeight: 200.h,
+                    elevation: 20,
+                    backgroundColor: Colors.white,
+                    leading: IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: const Icon(Icons.arrow_back),
+                    ),
+                    title: Text(
+                      "Bakeries and Pastries",
+                      style: Styles.styles16w700interFamily,
+                    ),
+                    actions: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10.w),
+                        child: SizedBox(
+                          width: 40.w,
+                          height: 50.h,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                top: 10.h,
+                                child: GestureDetector(
+                                  onTap: () {},
+                                  child: SvgPicture.asset(
+                                    "assets/svg_icons/favorite-Cart.svg",
+                                    height: 35.h,
+                                    width: 35.w,
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                right: 5.w,
+                                top: 5.h,
+                                child: Container(
+                                  height: 20.h,
+                                  width: 20.w,
+                                  decoration: const BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Color.fromRGBO(228, 144, 42, 1),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      "3",
+                                      style: Styles.styles11w600White.copyWith(
+                                        fontWeight: FontWeight.w700,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                    bottom: ButtonsTabBar(
+                      splashColor: MyColors.mainColor,
+                      height: 75.h,
+                      backgroundColor: const Color.fromRGBO(204, 229, 233, 1),
+                      unselectedBackgroundColor: Colors.white,
+                      unselectedBorderColor:
+                          const Color.fromRGBO(170, 170, 170, 1),
+                      labelStyle: Styles.styles15w400NormalBlack.copyWith(
+                          fontWeight: FontWeight.w600,
+                          color: MyColors.mainColor),
+                      borderColor: Colors.transparent,
+                      borderWidth: 1.sp,
+                      unselectedLabelStyle: Styles.styles15w400NormalBlack
+                          .copyWith(
+                              fontWeight: FontWeight.w500,
+                              color: const Color.fromRGBO(150, 150, 150, 1)),
+                      radius: 30.r,
+                      buttonMargin: EdgeInsets.all(10.sp),
+                      contentCenter: true,
+                      labelSpacing: 10.sp,
+                      contentPadding: EdgeInsets.all(12.sp),
+                      onTap: (index) => _scrollToSection(index),
+                      tabs: List.generate(
+                        categories.length,
+                        (index) => Tab(
+                          icon: Image.asset(images[index]),
+                          text: categories[index],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          body: Stack(
+            children: [
+              SingleChildScrollView(
+                controller: scrollController,
+                child: Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: List.generate(categories.length, (index) {
+                      return Column(
+                        key: sectionKeys[index],
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            categories[index],
+                            style: Styles.styles21w700black,
+                          ),
+                          SizedBox(height: 16.h),
+                          itemWidgetGridView(),
+                          SizedBox(height: 20.h),
+                        ],
+                      );
+                    }),
+                  ),
+                ),
+              ),
+              const Positioned(
+                bottom: 10,
+                left: 0,
+                right: 0,
+                child: CartContainer(
+                  svgIconPath: "assets/svg_icons/favorite-Cart.svg",
+                  buttonText: "Go To Cart",
+                  productsCount: "3",
+                  totalAmount: "310",
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
