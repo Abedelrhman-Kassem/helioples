92eb51d41439be8d2e44b7bbd5f3e04038691576
omar salah <96392338+Omar-Salah17@users.noreply.github.com>
2024-09-06 21:27:20 +0300

    main profile screen

--- lib/features/Profile/presentation/view/profile_screen.dart
+++ lib/features/Profile/presentation/view/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:negmt_heliopolis/core/constants/constants.dart';
 import 'package:negmt_heliopolis/core/utlis/theming/styles.dart';
 import 'package:negmt_heliopolis/features/Auth/SignUp/presentation/view/widgets/background_image.dart';
 import 'package:negmt_heliopolis/features/Profile/presentation/view/Widgets/profile_item.dart';
@@ -7,18 +8,51 @@ import 'package:negmt_heliopolis/features/Profile/presentation/view/Widgets/prof
 class ProfileScreen extends StatelessWidget {
   ProfileScreen({super.key});
 
-  List<ProfileItem> items = [
-    const ProfileItem(imagePath: "assets/svg_icons/profile_icon.svg", label: "Profile Information"),
-    const ProfileItem(imagePath: "assets/svg_icons/history_icon.svg", label: "Orders History"),
-    const ProfileItem(imagePath: "assets/svg_icons/alert_icon.svg", label: "Alerts"),
-    const ProfileItem(imagePath: "assets/svg_icons/settings_icon.svg", label: "App Settings"),
-    const ProfileItem(imagePath: "assets/svg_icons/help_center_icon.svg", label: "Help Center"),
-    const ProfileItem(imagePath: "assets/svg_icons/chat_icon.svg", label: "Chat with Us"),
-    const ProfileItem(imagePath: "assets/svg_icons/logout_icon.svg", label: "Logout"),
-  ];
-   
   @override
   Widget build(BuildContext context) {
+    // Define the items list inside the build method so you can use `context`
+    List<ProfileItem> items = [
+      ProfileItem(
+        imagePath: "assets/svg_icons/profile_icon.svg",
+        label: "Profile Information",
+        onTap: () {
+          Navigator.of(context).pushNamed(cahngeInformationScreen);
+        },
+      ),
+      ProfileItem(
+        imagePath: "assets/svg_icons/history_icon.svg",
+        label: "Orders History",
+        onTap: () {
+          Navigator.of(context).pushNamed(historyScreen);
+        },
+      ),
+      ProfileItem(
+        imagePath: "assets/svg_icons/alert_icon.svg",
+        label: "Alerts",
+        onTap: () {
+          Navigator.of(context).pushNamed(alertsScreen);
+        },
+      ),
+      ProfileItem(
+        imagePath: "assets/svg_icons/settings_icon.svg",
+        label: "App Settings",
+        onTap: () {
+          Navigator.of(context).pushNamed(settingsScreen);
+        },
+      ),
+      ProfileItem(
+        imagePath: "assets/svg_icons/help_center_icon.svg",
+        label: "Help Center",
+        onTap: () {
+          Navigator.of(context).pushNamed(helpCenterScreen);
+        },
+      ),
+      ProfileItem(
+          imagePath: "assets/svg_icons/chat_icon.svg", label: "Chat with Us"),
+      ProfileItem(
+          imagePath: "assets/svg_icons/logout_icon.svg", label: "Logout"),
+    ];
+
     return Scaffold(
       body: Stack(
         children: [
@@ -57,16 +91,17 @@ class ProfileScreen extends StatelessWidget {
               ),
               SliverPadding(
                 padding: EdgeInsets.symmetric(horizontal: 20.w),
-                sliver: 
-                 SliverGrid(
+                sliver: SliverGrid(
                   delegate: SliverChildBuilderDelegate(
                     (context, index) => items[index],
                     childCount: items.length,
                   ),
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 3,
-                    crossAxisSpacing: 8.0, // Optional: Add spacing between items
-                    mainAxisSpacing: 12.0,  // Optional: Add spacing between items
+                    crossAxisSpacing:
+                        8.0, // Optional: Add spacing between items
+                    mainAxisSpacing:
+                        12.0, // Optional: Add spacing between items
                   ),
                 ),
               ),
