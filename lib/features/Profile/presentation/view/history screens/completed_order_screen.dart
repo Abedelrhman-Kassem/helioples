942757fe83e2423ef1c7bfec5f9b2d628f8c45ac
omar salah <96392338+Omar-Salah17@users.noreply.github.com>
2024-10-30 13:18:24 +0300

    fetching comleted orders

--- lib/features/Profile/presentation/view/history screens/completed_order_screen.dart
+++ lib/features/Profile/presentation/view/history screens/completed_order_screen.dart
@@ -1,161 +1,220 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:negmt_heliopolis/core/constants/constants.dart';
 import 'package:negmt_heliopolis/core/utlis/theming/styles.dart';
+import 'package:negmt_heliopolis/features/Profile/presentation/view_model/history%20cubit/history_cubit.dart';
+import 'package:negmt_heliopolis/core/utlis/network/api_service.dart';
+import 'package:negmt_heliopolis/features/Profile/presentation/view_model/history%20cubit/history_states.dart';
+import '../../../data/repo/order%20history%20repo/order_history_repo_imp.dart';
 
-class CompletedOrderScreen extends StatelessWidget {
+class CompletedOrderScreen extends StatefulWidget {
   const CompletedOrderScreen({super.key});
 
   @override
+  _CompletedOrderScreenState createState() => _CompletedOrderScreenState();
+}
+
+class _CompletedOrderScreenState extends State<CompletedOrderScreen> {
+  late ScrollController _scrollController;
+  late HistoryCubit _historyCubit;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+
+    // Initialize and fetch history when the screen initializes
+    _historyCubit = HistoryCubit(OrderHistoryRepoImp(api: ApiService()));
+    _historyCubit.fetchHistory('OnWay');
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _historyCubit.fetchHistory('OnWay');
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ListView.builder(
-            itemCount: 5,
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 12.h, horizontal: 26.w),
-                    child: Text(
-                      "Today",
-                      style: Styles.styles13w400interFamily
-                          .copyWith(color: const Color.fromRGBO(41, 41, 41, 1)),
-                    ),
-                  ),
-                  ListView.builder(
-                    itemCount: 2,
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: 22.h, vertical: 8.h),
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).pushNamed(orderDetailsScreen);
-                          },
-                          child: Container(
-                            width: 160.w,
-                            height: 130.h,
-                            decoration: BoxDecoration(
-                                color: const Color.fromRGBO(227, 227, 227, 1),
-                                borderRadius: BorderRadius.circular(30.r),
-                                border: Border.all(
-                                    color: const Color.fromRGBO(
-                                        243, 243, 243, 1))),
-                            child: Row(
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.symmetric(
-                                      horizontal: 15.w, vertical: 15.h),
-                                  child: Image.asset(
-                                    "assets/Icons_logos/history_icon.png",
-                                    width: 60.w,
-                                    height: 60.h,
-                                  ),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.symmetric(vertical: 20.h),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Row(
-                                        children: [
-                                          Text.rich(
-                                            TextSpan(
-                                                text: "12 ",
-                                                style: Styles
-                                                    .styles13w300interFamily
-                                                    .copyWith(
-                                                        fontWeight:
-                                                            FontWeight.bold,
-                                                        color: Colors.black),
+    return BlocProvider(
+      create: (context) => _historyCubit,
+      child: BlocBuilder<HistoryCubit, FetchHistoryState>(
+        builder: (context, state) {
+          if (state is FetchHistoryLoading &&
+              _historyCubit.orderHistoryList.isEmpty) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is FetchHistoryFailure) {
+            return Center(child: Text(state.msg));
+          } else if (state is FetchHistorySuccess ||
+              _historyCubit.orderHistoryList.isNotEmpty) {
+            return ListView.builder(
+              controller: _scrollController,
+              itemCount: _historyCubit.orderHistoryList.length + 1,
+              itemBuilder: (context, index) {
+                if (index == _historyCubit.orderHistoryList.length) {
+                  return _historyCubit.isFetching
+                      ? const Center(child: CircularProgressIndicator())
+                      : const SizedBox.shrink();
+                }
+
+                // Render each OrderHistory item
+                var orderHistory = _historyCubit.orderHistoryList[index];
+                return Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 22.w, vertical: 8.h),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Display the date
+                      Text(
+                        orderHistory.date ?? '',
+                        style: Styles.styles17w700interFamily.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 8.h),
+
+                      Column(
+                        children: orderHistory.orders?.map((order) {
+                              return Padding(
+                                padding: EdgeInsets.only(bottom: 8.h),
+                                child: GestureDetector(
+                                  onTap: () {
+                                    Navigator.of(context)
+                                        .pushNamed(orderDetailsScreen);
+                                  },
+                                  child: Container(
+                                    width: 400.w,
+                                    height: 130.h,
+                                    decoration: BoxDecoration(
+                                      color: const Color.fromRGBO(
+                                          227, 227, 227, 1),
+                                      borderRadius: BorderRadius.circular(30.r),
+                                      border: Border.all(
+                                        color: const Color.fromRGBO(
+                                            243, 243, 243, 1),
+                                      ),
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        Padding(
+                                          padding: EdgeInsets.symmetric(
+                                              horizontal: 15.w, vertical: 15.h),
+                                          child: Image.asset(
+                                            "assets/Icons_logos/history_icon.png",
+                                            width: 60.w,
+                                            height: 60.h,
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.symmetric(
+                                              vertical: 20.h),
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Row(
                                                 children: [
-                                                  TextSpan(
-                                                      text: "Items",
+                                                  Text.rich(
+                                                    TextSpan(
+                                                      text: "${order.userId} ",
                                                       style: Styles
                                                           .styles13w300interFamily
                                                           .copyWith(
-                                                              color: const Color
-                                                                  .fromRGBO(80,
-                                                                  80, 80, 1)))
-                                                ]),
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .bold,
+                                                              color:
+                                                                  Colors.black),
+                                                      children: [
+                                                        TextSpan(
+                                                          text: "Items",
+                                                          style: Styles
+                                                              .styles13w300interFamily
+                                                              .copyWith(
+                                                                  color: const Color
+                                                                      .fromRGBO(
+                                                                      80,
+                                                                      80,
+                                                                      80,
+                                                                      1)),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                  ),
+                                                  SizedBox(width: 125.w),
+                                                  Text(
+                                                    "${order.total!.toStringAsFixed(2)} EGP",
+                                                    style: Styles
+                                                        .styles17w700interFamily,
+                                                  ),
+                                                ],
+                                              ),
+                                              SizedBox(height: 8.h),
+                                              Row(
+                                                children: [
+                                                  SvgPicture.asset(
+                                                      "assets/svg_icons/location_history.svg"),
+                                                  SizedBox(width: 6.w),
+                                                  Text(
+                                                    "13 Salah Salem, No 45 Cairo Egypt",
+                                                    style: Styles
+                                                        .styles12w400interFamily
+                                                        .copyWith(
+                                                            color: const Color
+                                                                .fromRGBO(
+                                                                80, 80, 80, 1)),
+                                                  ),
+                                                ],
+                                              ),
+                                              SizedBox(height: 8.h),
+                                              Row(
+                                                children: [
+                                                  Image.asset(
+                                                      "assets/Icons_logos/logos_visa.png"),
+                                                  SizedBox(width: 6.w),
+                                                  Text(
+                                                    "***521",
+                                                    style: Styles
+                                                        .styles11w700interFamily
+                                                        .copyWith(
+                                                            color: Colors.black,
+                                                            fontWeight:
+                                                                FontWeight.bold,
+                                                            fontSize: 13.sp),
+                                                  ),
+                                                ],
+                                              ),
+                                            ],
                                           ),
-                                          SizedBox(
-                                            width: 125.w,
-                                          ),
-                                          Text(
-                                            "325.00EGP",
-                                            style:
-                                                Styles.styles17w700interFamily,
-                                          ),
-                                        ],
-                                      ),
-                                      SizedBox(
-                                        height: 8.h,
-                                      ),
-                                      Row(
-                                        children: [
-                                          SvgPicture.asset(
-                                              "assets/svg_icons/location_history.svg"),
-                                          SizedBox(
-                                            width: 6.w,
-                                          ),
-                                          Text(
-                                            "13 Salah Salem, No 45 Cairo Egypt",
-                                            style: Styles
-                                                .styles12w400interFamily
-                                                .copyWith(
-                                                    color: const Color.fromRGBO(
-                                                        80, 80, 80, 1)),
-                                          )
-                                        ],
-                                      ),
-                                      SizedBox(
-                                        height: 8.h,
-                                      ),
-                                      Row(
-                                        children: [
-                                          Image.asset(
-                                              "assets/Icons_logos/logos_visa.png"),
-                                          SizedBox(
-                                            width: 6.w,
-                                          ),
-                                          Text(
-                                            "***521",
-                                            style: Styles
-                                                .styles11w700interFamily
-                                                .copyWith(
-                                                    color: Colors.black,
-                                                    fontWeight: FontWeight.bold,
-                                                    fontSize: 13.sp),
-                                          )
-                                        ],
-                                      )
-                                    ],
+                                        ),
+                                      ],
+                                    ),
                                   ),
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
-                    },
+                                ),
+                              );
+                            }).toList() ??
+                            [],
+                      ),
+                    ],
                   ),
-                ],
-              );
-            },
-          ),
-        ],
+                );
+              },
+            );
+          }
+
+          return const SizedBox.shrink();
+        },
       ),
     );
   }
