466f036e0efb8886f119b8cc8c4de804d132e858
omar salah <96392338+Omar-Salah17@users.noreply.github.com>
2024-11-28 14:42:09 +0200

    location search ui
    change the location search ui

--- lib/features/Auth/SignUp/presentation/view/widgets/place_item.dart
+++ lib/features/Auth/SignUp/presentation/view/widgets/place_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:negmt_heliopolis/core/utlis/theming/styles.dart';
 
 import '../../../data/model/place_suggestion.dart';
 
@@ -11,50 +12,53 @@ class PlaceItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // ignore: unused_local_variable
     var subTitle = suggestion.description
         .replaceAll(suggestion.description.split(',')[0], '');
-    return Container(
-      width: double.infinity,
-      margin: const EdgeInsetsDirectional.all(8),
-      padding: const EdgeInsetsDirectional.all(4),
-      decoration: BoxDecoration(
-          color: Colors.white, borderRadius: BorderRadius.circular(8)),
-      child: Column(
-        children: [
-          ListTile(
-            leading: Container(
-              width: 40,
-              height: 40,
-              decoration: const BoxDecoration(
-                  shape: BoxShape.circle, color: Colors.lightBlue),
-              child: const Icon(
-                Icons.place,
-                color: Colors.blue,
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          margin: const EdgeInsetsDirectional.all(12),
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(8)),
+          child: Row(
+            children: [
+              SvgPicture.asset("assets/svg_icons/location_drop_Icon.svg"),
+              SizedBox(
+                width: 12.w,
               ),
-            ),
-            title: RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: '${suggestion.description.split(',')[0]}\n',
-                    style: const TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  TextSpan(
-                    text: subTitle.substring(2),
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                      '${suggestion.description.split(',')[0]}',
+                      style: Styles.styles14w400NormalBlack,
+                    ), 
                     ),
-                  )
-                ],
+                   
+                    Text(
+                      '${suggestion.description.split(',')[1]}',
+                      style: Styles.styles12w400black
+                          .copyWith(color: const Color(0xFF7E7E7E)),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
-      ),
+            ],
+          ),
+        ),
+        Divider(
+          thickness: 1, 
+          height: 16.h, 
+          color: const  Color(0xFFADADAD), 
+          indent: 16.w, 
+          endIndent: 16.w, 
+        ),
+      ],
     );
   }
 }
