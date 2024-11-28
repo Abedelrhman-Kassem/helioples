c5810953b9a37a88f7d9a5c9a373b284047c5080
omar salah <96392338+Omar-Salah17@users.noreply.github.com>
2024-11-28 14:53:29 +0200

    login ui is done
    modify the login ui to be like  figma

--- lib/features/Auth/Login/data/repo/log_in_repo.dart
+++ lib/features/Auth/Login/data/repo/log_in_repo.dart
@@ -1,6 +1,5 @@
 import 'package:dartz/dartz.dart';
 import 'package:negmt_heliopolis/core/utlis/errors/failure.dart';
-import 'package:negmt_heliopolis/features/Auth/SignUp/data/model/user.dart';
 
 abstract class LogInRepo
 {
