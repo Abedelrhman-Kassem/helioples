d478a53f23d9937335005398de7e04047b75811e
Abedelrhman-Kassem <107558049+Abedelrhman-Kassem@users.noreply.github.com>
2025-02-11 13:25:57 +0200

    fix category name in product screen

--- lib/features/Product/presentation/view/product_screen.dart
+++ lib/features/Product/presentation/view/product_screen.dart
@@ -21,157 +21,168 @@ class ProductScreen extends StatefulWidget {
 }
 
 class _ProductScreenState extends State<ProductScreen> {
+  String title = '';
+
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => ProductCubit()..getProductDetails(widget.productId),
-      child: Scaffold(
-        appBar: AppBar(
-          leading: returnArrow(
-            context: context,
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          title: const CategoriesBottomSheet(
-            title: 'Bakeries And Pastries',
-          ),
-          actions: const [
-            CartCounter(),
-          ],
-        ),
-        body: BlocConsumer<ProductCubit, ProductState>(
-          listener: (context, state) {},
-          builder: (context, state) {
-            if (state is GetProductSuccess) {
-              return SingleChildScrollView(
-                physics: const BouncingScrollPhysics(),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: ProductWidget(
-                        product: state.productModel.product!,
-                      ),
-                    ),
-                    SizedBox(height: 30.h),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                      child: Text(
-                        'Related Products',
-                        style: Styles.styles16w600NormalBlack,
-                      ),
-                    ),
-                    SizedBox(height: 10.h),
-                    SizedBox(
-                      height: 210,
-                      child: ListView(
-                        physics: const BouncingScrollPhysics(),
-                        scrollDirection: Axis.horizontal,
+      child: BlocConsumer<ProductCubit, ProductState>(
+        listener: (context, state) {
+          if (state is GetProductSuccess) {
+            title = state.productModel.category!.name!;
+          }
+        },
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(
+              leading: returnArrow(
+                context: context,
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              title: CategoriesBottomSheet(
+                title: title,
+              ),
+              actions: const [
+                CartCounter(),
+              ],
+            ),
+            body: Column(
+              children: [
+                if (state is GetProductSuccess)
+                  Expanded(
+                    child: SingleChildScrollView(
+                      physics: const BouncingScrollPhysics(),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          const SizedBox(width: 20),
-                          ListView.separated(
-                            physics: const NeverScrollableScrollPhysics(),
-                            shrinkWrap: true,
-                            scrollDirection: Axis.horizontal,
-                            itemBuilder: (context, index) => ItemWidget(
-                              relatedProductsModel:
-                                  state.productModel.related![index],
+                          Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: ProductWidget(
+                              product: state.productModel.product!,
                             ),
-                            separatorBuilder: (context, index) =>
-                                SizedBox(width: 10.w),
-                            itemCount: state.productModel.related!.length,
                           ),
-                          const SizedBox(width: 20),
+                          SizedBox(height: 30.h),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 20.0),
+                            child: Text(
+                              'Related Products',
+                              style: Styles.styles16w600NormalBlack,
+                            ),
+                          ),
+                          SizedBox(height: 10.h),
+                          SizedBox(
+                            height: 210,
+                            child: ListView(
+                              physics: const BouncingScrollPhysics(),
+                              scrollDirection: Axis.horizontal,
+                              children: [
+                                const SizedBox(width: 20),
+                                ListView.separated(
+                                  physics: const NeverScrollableScrollPhysics(),
+                                  shrinkWrap: true,
+                                  scrollDirection: Axis.horizontal,
+                                  itemBuilder: (context, index) => ItemWidget(
+                                    relatedProductsModel:
+                                        state.productModel.related![index],
+                                  ),
+                                  separatorBuilder: (context, index) =>
+                                      SizedBox(width: 10.w),
+                                  itemCount: state.productModel.related!.length,
+                                ),
+                                const SizedBox(width: 20),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 100.h),
                         ],
                       ),
                     ),
-                    SizedBox(height: 100.h),
-                  ],
-                ),
-              );
-            }
-            if (state is GetProductLoading) {
-              return Skeletonizer(
-                child: Padding(
-                  padding: EdgeInsets.all(20.0.r),
-                  child: Column(
-                    children: [
-                      Container(
-                        width: double.infinity,
-                        height: 400.h,
-                        color: Colors.grey,
-                      ),
-                      SizedBox(height: 50.h),
-                      SizedBox(
-                        height: 240,
-                        child: ListView(
-                          scrollDirection: Axis.horizontal,
-                          children: List.generate(
-                            7,
-                            (index) => Container(
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 25,
-                                horizontal: 10,
-                              ),
-                              margin: const EdgeInsets.symmetric(
-                                horizontal: 5,
-                              ),
-                              height: 240,
-                              width: 130,
-                              decoration: BoxDecoration(
-                                color: Colors.grey[100],
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  const Row(
-                                    mainAxisAlignment: MainAxisAlignment.end,
+                  ),
+                if (state is GetProductLoading)
+                  Skeletonizer(
+                    child: Padding(
+                      padding: EdgeInsets.all(20.0.r),
+                      child: Column(
+                        children: [
+                          Container(
+                            width: double.infinity,
+                            height: 400.h,
+                            color: Colors.grey,
+                          ),
+                          SizedBox(height: 50.h),
+                          SizedBox(
+                            height: 240,
+                            child: ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: List.generate(
+                                7,
+                                (index) => Container(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 25,
+                                    horizontal: 10,
+                                  ),
+                                  margin: const EdgeInsets.symmetric(
+                                    horizontal: 5,
+                                  ),
+                                  height: 240,
+                                  width: 130,
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[100],
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
                                     children: [
-                                      Icon(Icons.heart_broken),
+                                      const Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.end,
+                                        children: [
+                                          Icon(Icons.heart_broken),
+                                        ],
+                                      ),
+                                      Helper.loadNetworkImage(
+                                        assetsErrorPath:
+                                            'assets/screens_background/home-category.png',
+                                      ),
+                                      const Text('hello there'),
                                     ],
                                   ),
-                                  Helper.loadNetworkImage(
-                                    assetsErrorPath:
-                                        'assets/screens_background/home-category.png',
-                                  ),
-                                  const Text('hello there'),
-                                ],
+                                ),
                               ),
                             ),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              );
-            }
-            if (state is GetProductFailure) {
-              return Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(state.error),
-                    TextButton(
-                      onPressed: () {
-                        BlocProvider.of<ProductCubit>(context)
-                            .getProductDetails(widget.productId);
-                      },
-                      child: const Text('Tap to try again'),
+                          )
+                        ],
+                      ),
                     ),
-                  ],
-                ),
-              );
-            }
-            return const SizedBox();
-          },
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: const CartContainer(),
+                  ),
+                if (state is GetProductFailure)
+                  Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(state.error),
+                        TextButton(
+                          onPressed: () {
+                            BlocProvider.of<ProductCubit>(context)
+                                .getProductDetails(widget.productId);
+                          },
+                          child: const Text('Tap to try again'),
+                        ),
+                      ],
+                    ),
+                  ),
+              ],
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+            floatingActionButton: const CartContainer(),
+          );
+        },
       ),
     );
   }
